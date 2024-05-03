8d67f47476f3ddb8848b99179c337541d72ae4e2
varioustoxins <garyt.and.sarahb@gmail.com>
2024-05-03 17:24:52 +0100

    main program script needed arguments and error messages

--- src/ccpn_project_checker/main.py
+++ src/ccpn_project_checker/main.py
@@ -1,8 +1,16 @@
 from ccpn_project_checker.DiskModelChecker import run_cli_checker
 import sys
 
-def main():
-    result = run_cli_checker()
+def main(file_path:str ):
+
+
+    result = run_cli_checker(sys.argv[1])
 
     sys.exit(result)
 
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: ccpn_project_checker <path_to_project>")
+        sys.exit(1)
+
+    main(sys.argv[1])
